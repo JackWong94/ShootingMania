0cb2bd0ce76a2fe50ed30437c54f3850cc1fe04f
JackWong94 <w.yuheng94@gmail.com>
2023-01-04 00:41:16 +0800

    Enhance performance within sprite by removing repeatedly bitmap decode

--- app/src/main/java/com/example/shootingmania/BulletMarks.java
+++ app/src/main/java/com/example/shootingmania/BulletMarks.java
@@ -2,25 +2,31 @@ package com.example.shootingmania;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.Log;
 
 import java.util.ConcurrentModificationException;
 import java.util.Random;
 
 public class BulletMarks {
-    private int numberOfSprites = 3;
+    static boolean resourcesLoaded = false;
+    static private int numberOfSprites = 3;
     private Random random;
     public int posX, posY;
     private int frame = 0;
 
 
-    private Bitmap bulletMarks[] = new Bitmap[numberOfSprites];
+    static private Bitmap bulletMarks[] = new Bitmap[numberOfSprites];
 
     public BulletMarks(Context context, int posX, int posY) {
         this.posX = posX;
         this.posY = posY;
-        bulletMarks[0] = Sprite.createSprite(context, Sprite.NAME.BULLET_MARKS_1);
-        bulletMarks[1] = Sprite.createSprite(context, Sprite.NAME.BULLET_MARKS_2);
-        bulletMarks[2] = Sprite.createSprite(context, Sprite.NAME.BULLET_MARKS_3);
+
+        if (!resourcesLoaded) {
+            bulletMarks[0] = Sprite.createSprite(context, Sprite.NAME.BULLET_MARKS_1);
+            bulletMarks[1] = Sprite.createSprite(context, Sprite.NAME.BULLET_MARKS_2);
+            bulletMarks[2] = Sprite.createSprite(context, Sprite.NAME.BULLET_MARKS_3);
+            resourcesLoaded = true;
+        }
         random = new Random();
         frame = random.nextInt(numberOfSprites);
     }
